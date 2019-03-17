f12b70e1e495e83789a266ce26f83b5300164134
Tom Maeda <624.mdasan@gmail.com>
2019-03-17 21:13:42 +0900

    add: setting

---
+++ development.js
@@ -0,0 +1,31 @@
+
+
+
+const src = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'dist')
+
+export default {
+    mode: 'development',
+    entry: src + '/index.jsx',
+
+    output: {
+        path: dist,
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test:/\.jsx$/,
+                exclude:/node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    plugin: []
+}
