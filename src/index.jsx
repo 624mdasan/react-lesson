5785fccfec7e90687d9b377192ac4382dec78372
Tom Maeda <624.mdasan@gmail.com>
2019-03-20 21:44:50 +0900

    setting

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { message: 'something' }
+    }
+
+    onChange(e) {
+        this.setState( { message: e.target.value} )
+    }
+
     render () {
-        return <p> Hello React!</p>;
+        return (
+            <div>
+                <input type="text" onChange = { this.onChange.bind(this) } />
+                <p>{ this.state.message }</p>
+            </div>
+        )
     }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById('app'))
